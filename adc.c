b1994ad899b4bc81aafc1ca220e4af1b68f9f212
Nate Christianson <natetc95@email.arizona.edu>
2016-03-24 17:55:11 -0700

    Doesnt work. I blame Jordan.

---
+++ adc.c
@@ -0,0 +1,33 @@
+#include <xc.h>
+#include "adc.h"
+
+void initADC(void){
+    
+    //ANSELBbits.ANSB0 = 0; 
+    AD1CON1bits.FORM = 0; // 16 unsigned integer
+    AD1CON1bits.SSRC = 7; // Auto-convert mode
+    AD1CON1bits.ASAM = 1; // Auto-sampling
+    AD1CON2bits.VCFG = 0; // Use board refernece voltages
+    AD1CON2bits.CSCNA = 0; // Disable scanning
+    AD1CON2bits.SMPI = 0; // 1 burrito
+    AD1CON2bits.ALTS = 0; // Only Mux A
+    AD1CON3bits.ADRC = 0; // Use PBCLK
+    AD1CON3bits.SAMC = 2; // 2 Tad per sample
+    AD1CON3bits.ADCS = 1; // 4 times the PBCLK
+    AD1CHSbits.CH0NA = 0; // Use Vref- as negative reference
+    AD1CHSbits.CH0SA = 0; // Scan AN0 at least
+    IFS0bits.AD1IF = 0; // Put down ADC flag
+    IPC5bits.AD1IP = 7;
+    IEC0bits.AD1IE = 0;
+    ADC1BUFF = 0;
+    AD1CON1bits.ADON = 1; // turn on the ADC
+    
+}
+
+int analogRead(void){
+    AD1CON1bits.SAMP = 1;           // Begin sampling
+    while( AD1CON1bits.SAMP );      // wait until acquisition is done
+    while( ! AD1CON1bits.DONE );    // wait until conversion done
+ 
+    return ADC1BUF0;                // result stored in ADC1BUF0
+}
---
+++ main.c
@@ -0,0 +1,121 @@
+// ******************************************************************************************* //
+//
+// File:         lab1p2.c
+// Date:         12-30-2014
+// Authors:      Garrett Vanhoy
+//
+// Description: 
+// ******************************************************************************************* //
+
+#include <xc.h>
+#include <sys/attribs.h>
+#include "lcd.h"
+#include "timer.h"
+#include "config.h"
+#include "interrupt.h"
+#include "switch.h"
+#include "keypad.h"
+#include "adc.h"
+
+
+#define CLR 0x01
+
+#define RUN_LED LATDbits.LATD2
+#define STP_LED LATDbits.LATD0
+#define RUN TRISDbits.TRISD2
+#define STP TRISDbits.TRISD0
+
+#define SWT_T TRISDbits.TRISD5
+#define SWT PORTDbits.RD5
+
+#define RST_T TRISDbits.TRISD6
+#define RST PORTDbits.RD6
+
+#define ROW_1 LATDbits.LATD12
+#define ROW_2 LATDbits.LATD6
+#define ROW_3 LATDbits.LATD5
+#define ROW_4 LATDbits.LATD11
+
+#define COL_1 PORTGbits.RG0
+#define COL_2 PORTGbits.RG13
+#define COL_3 PORTGbits.RG12
+
+
+// ******************************************************************************************* //
+
+typedef enum RS_enum {
+    wait, check, write, changeM1, changeM2
+}status;
+
+volatile int q = 0;
+volatile int d = 0;
+volatile int line = 1;
+volatile unsigned int val = 0;
+volatile unsigned int digitalValue = 0;
+volatile char k = -1;
+volatile int x = 0;
+volatile char password[5] = {' ', ' ', ' ', ' ', '\0'};
+volatile int place = 0;
+
+volatile char setCheck[5] = {'*', '*', ' ', ' ', '\0'};
+volatile char invalid1 = '*';
+volatile char invalid2 = '#';
+
+volatile status state = wait;
+volatile status next;
+
+int main(void)
+{
+    enableInterrupts();
+    
+    
+    SYSTEMConfigPerformance(10000000);
+    STP = 0;
+    RUN = 0;
+    int i;
+    
+    LATDbits.LATD0 = 1;
+    initTimer2();
+    initLCD();
+    initADC();
+    delayMs(1000);
+    LATDbits.LATD0 = 0;
+
+    TRISDbits.TRISD0 = 0;
+    
+    writeCMD(CLR);
+    //clearLCD();
+    moveCursorLCD(0,2);
+    testLCD();
+    
+    LATDbits.LATD0 = 1;
+    delayMs(1000);
+            
+    printStringLCD("INIT COMPLETE!");
+    
+    delayMs(2000);
+    
+    char *a;
+    
+    int l = 0;
+    
+    while(1)
+    {   
+        delayMs(200);
+        writeCMD(CLR);
+        digitalValue=(1023*ADC1BUF0)/3.3;
+        if(ADC1BUF0 > 512) {
+            printStringLCD("1");
+        }
+        else {
+            printCharLCD('0');
+        }
+        
+    }
+    return 0;
+}
+
+void __ISR(23, IPL7AUTO) _ADCInterrupt(void){
+    IFS0bits.AD1IF = 0;
+    val = ADC1BUF0;
+}
